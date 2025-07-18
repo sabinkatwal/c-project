6ac2f2f77b0fba68e9b54aafbf25d900a6c173c1
WattAboutIt <081bei030.safal@pcampus.edu.np>
2025-07-18 22:48:25 +0545

    Add the whole game

---
+++ angrybird-clone/src/Bird.hpp
@@ -0,0 +1,28 @@
+// Bird.hpp
+#ifndef BIRD_HPP
+#define BIRD_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Bird {
+public:
+    Bird(float startX, float startY);
+
+    void launch(float initialVelX, float initialVelY);
+    void update(sf::Time deltaTime);
+    void draw(sf::RenderWindow& window);
+    void reset(float startX, float startY);
+
+    sf::Vector2f getPosition() const;
+    sf::FloatRect getBounds() const;
+    bool isFlying() const;
+
+private:
+    sf::CircleShape shape;
+    sf::Vector2f velocity;
+    bool flying;
+    sf::Vector2f initialPosition; // To reset the bird
+    const float gravity = 980.0f; // Pixels per second squared (simplified)
+};
+
+#endif // BIRD_HPP
